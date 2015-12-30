65eece0b7eb9e1dd6fad154229087a395610247c
Sven Mkw <sven.mkw@gmail.com>
2015-12-30 15:31:57 +0100

    Initial Commit

---
+++ iOSLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSLog
+//
+//  Created by Sven Mischkewitz on 30/12/15.
+//  Copyright © 2015 Sven Mischkewitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
