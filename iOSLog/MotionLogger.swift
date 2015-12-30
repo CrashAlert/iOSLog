4930e5e2065fa222cbb6ea817e41390a2b138874
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2015-12-30 18:20:39 +0100

    New file for MotionLogger

--- iOSLog/MotionLogger.swift
+++ iOSLog/MotionLogger.swift
@@ -6,3 +6,66 @@
 //  Copyright © 2015 Sven Mischkewitz. All rights reserved.
 //
 
+import CoreMotion
+
+class MotionLogger {
+    // CLASS VARIABLES
+    static let sharedInstance = MotionLogger()
+    
+    
+    // INSTANCE VARIABLES
+    let motionManager = CMMotionManager()
+    let gyroUpdateInterval = 0.2
+    let accUpdateInterval = 0.2
+    
+    init() {
+        // configure motion manager
+        motionManager.gyroUpdateInterval = gyroUpdateInterval
+        motionManager.accelerometerUpdateInterval = accUpdateInterval
+        
+        NSLog("\(motionManager)")
+    }
+    
+    func start() {
+        NSLog("Start Motion Manager")
+        self.startAccelerationLog();
+    }
+    
+    func startAccelerationLog() {
+        NSLog("Start Acceleration Logging")
+        
+        if (!motionManager.accelerometerAvailable) {
+            NSLog("Accelerometer not available!")
+            return
+        }
+        
+        motionManager.startAccelerometerUpdates()
+        motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!) {
+            (accelerometerData: CMAccelerometerData?, NSError) -> Void in
+            self.logAccelerationData(accelerometerData!.acceleration)
+            if (NSError != nil) {
+                print("\(NSError)")
+            }
+        }
+    }
+    
+    //    func startGyroLog() {
+    //        motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()!) {
+    //
+    //        }
+    //    }
+    
+    func stopAccelerationLog() {
+        motionManager.stopAccelerometerUpdates()
+    }
+    
+    func stop() {
+        
+    }
+    
+    func logAccelerationData(acceleration: CMAcceleration) {
+        let strX = String(format: "%.10f", acceleration.x)
+        let strY = String(format: "%.10f", acceleration.y)
+        let strZ = String(format: "%.10f", acceleration.z)
+    }
+}
