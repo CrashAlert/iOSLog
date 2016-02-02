1f72e50258e01becf5b60da02f9a4b907dee7abc
Sven Mkw <sven.mkw@gmail.com>
2016-02-02 02:02:13 +0100

    add explicit background mode calls for ios9

--- iOSLog/MotionLogger.swift
+++ iOSLog/MotionLogger.swift
@@ -102,6 +102,9 @@ class MotionLogger: NSObject, CLLocationManagerDelegate  {
             return
         }
         
+        if #available(iOS 9.0, *) {
+            locationManager.allowsBackgroundLocationUpdates = true
+        }
         locationManager.startUpdatingLocation()
     }
     
@@ -194,7 +197,12 @@ class MotionLogger: NSObject, CLLocationManagerDelegate  {
     }
     
     func stopGPSLog() {
+        NSLog("Stop GPS")
+        
         locationManager.stopUpdatingLocation()
+        if #available(iOS 9.0, *) {
+            locationManager.allowsBackgroundLocationUpdates = false
+        }
     }
     
     func stopMagnetometerLog() {
